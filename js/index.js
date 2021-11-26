a0b282c2b5ec94d640a0cf1867df91489b958d78
mateusbr85 <mateus.13061903@gmail.com>
2021-11-25 23:37:26 -0300

    fazendo o form

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
     function pintaAtrasado (data,prazo) {
       console.log("sua data dentro da function é ",data);
       var limite = prazo;
-      if (data >= limite * 0 && data < limite *0.30) {
-        document.getElementById("oi").setAttribute("class","vermelho");
+      if (data >= limite * 0.70 && data < limite *1) {
+        document.getElementById("oi").setAttribute("class","normal");
         }
       if(data >= limite * 0.30 && data < limite *0.70) {
         document.getElementById("oi").setAttribute("class","amarelo");
       }
-      if(data >= limite * 0.70) {
-        document.getElementById("oi").setAttribute("class","teste");
+      if(data >= limite * 0 && data < limite * 0.30) {
+        document.getElementById("oi").setAttribute("class","aVencer");
+      }else {
+        document.getElementById("oi").setAttribute("class","vencido");
       }
   };
 
 var prazo = 5;
-var dataHoje = "22/11/2021";//moment().format("DD/MM/YYYY");
+var dataHoje = moment().format("DD/MM/YYYY");
 console.log(dataHoje);
-var dataPrazo = "22/11/2021";
+var dataPrazo = "30/11/2021";
 
 var diff = moment(dataPrazo,"DD/MM/YYYY").diff(moment(dataHoje,"DD/MM/YYYY"));
 
